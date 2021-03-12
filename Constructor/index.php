d8e64cbd13570cc4f7d01955adb033766b505d85
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-03-12 09:55:22 +0600

    constructor function

---
+++ Constructor/index.php
@@ -0,0 +1,32 @@
+<?php
+
+class person{
+
+    public $name;
+    public $age;
+
+    function __construct($name ="No Name", $age = 0)
+    {
+        $this->name =$name;
+        $this->age =$age;
+    }
+
+    function show(){
+        echo $this->name. " - ". $this->age . "<br>";
+    }
+}
+
+$p1 = new person();
+$p2 = new person("Raihan Mahmudi", 20);
+$p3 = new person("Hasan Mahmud", 15);
+$p4 = new person("Rabbi Hasan", 18);
+
+// $p1->name = "Raihan Mahmudi";
+// $p1->age = 20;
+$p1->show();
+$p2->show();
+$p3->show();
+$p4->show();
+
+
+?>
